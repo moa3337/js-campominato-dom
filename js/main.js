034ad49f853b22e3623325dfb542c57ad17c892c
moa3337 <moa-x@hotmail.com>
2023-03-04 14:24:23 +0100

    correzione 3 final commit

--- js/main.js
+++ js/main.js
@@ -11,10 +11,11 @@ const button = document.getElementById("generate");
 
 // DICHIARO UN CONTATORE PER RACCOGLIERE IL PUNTEGGIO
 let score = 0;
+let gameOver = false;
 
 // CREO L'EVENTO AL "CLICK" DEL BOTTONE
 button.addEventListener(
-    "click", 
+    "click",
     function () {
 
         // AGGANCIO AL DIV DEL HTML
@@ -36,50 +37,50 @@ function generateGrid(grid) {
 
         // CREO UN DIV CON NOME SQUARE PER LE CASELLE
         const square = document.createElement("div");
-        
+
         // DO LA CLASSE "square" AL DIV
         square.classList.add("square");
 
         // SCRIVO I NUMERI DA 1 A 100 NEGLI SQUARES
         square.innerHTML = i;
-        
+
         // CREO UN EVENTO SUL "CLICK" DI OGNI SQUARE
         square.addEventListener(
             "click",
             function () {
+                if (!gameOver) {
+                    // SE LA CASELLA CONTIENE UN "numberBomb" COLORA DI ROSSO
+                    if (numberBomb.includes(i)) {
+                        square.classList.add("bomb");
 
-                // SE LA CASELLA CONTIENE UN "numberBomb" COLORA DI ROSSO
-                if (numberBomb.includes(i)) {
-                    square.classList.add("bomb");
+                        // E STAMPO IL PUNTEGGIO FATTO DALL'UTENTE
+                        console.log("GAME OVER! hai fatto " + score + " punti");
+                        gameOver = true;
+                        // ALTRIMENTI COLORA NORMALMENTE E CONTA I PUNTI 
+                    } else {
+                        this.classList.add("active");
+                        score++;
+                    }
+                }
 
-                    // E STAMPO IL PUNTEGGIO FATTO DALL'UTENTE
-                    console.log("GAME OVER! hai aftto " + score + " punti");
-                  
-                    // ALTRIMENTI COLORA NORMALMENTE E CONTA I PUNTI 
-                } else {
-                    this.classList.add("active"); score++;
-                }  
-
-                // QUANDO CLICCO SU UNO SQUARE STAMPALO IN CONSOLE
-                console.log("casella selezionata n°:" + i);
             });
-            grid.append(square);
-        }   
-
-        // ARRAY PER I NUMERI CASUALI CHE CONTERRANNO LE BOMBE
-        const numberBomb = []
-
-        // CICLO "while" PER GENERARE 16 NUMERI/BOMBE CASUALI
-        while (numberBomb.length < 16) {
-            randomNumber = Math.floor(Math.random() * 100) + 1;
-
-            // SE 1 DEI NUMERI GENERATI E' GIA' PRESENTE 
-            // SALTA IL CICLO E' CREANE 1 NUOVO
-            if (!numberBomb.includes(randomNumber)) {
-                numberBomb.push(randomNumber);
-            }
-        }
-        // STAMPO IN CONSOLE IL NUMERO/BOMBA
-        console.log(numberBomb);
+        grid.append(square);
     }
+
+    // ARRAY PER I NUMERI CASUALI CHE CONTERRANNO LE BOMBE
+    const numberBomb = []
+
+    // CICLO "while" PER GENERARE 16 NUMERI/BOMBE CASUALI
+    while (numberBomb.length < 16) {
+        randomNumber = Math.floor(Math.random() * 100) + 1;
+
+        // SE 1 DEI NUMERI GENERATI E' GIA' PRESENTE 
+        // SALTA IL CICLO E' CREANE 1 NUOVO
+        if (!numberBomb.includes(randomNumber)) {
+            numberBomb.push(randomNumber);
+        }
+    }
+    // STAMPO IN CONSOLE IL NUMERO/BOMBA
+    console.log(numberBomb);
+}
 
